52978494dba197b887202aa5f2abcac26a67a4b1
Daniel LaPrade <daniellaprade1@gmail.com>
2026-04-22 14:40:06 -0400

    Updated profile and repository entities

--- backend/src/main/java/com/daniellaprade1/self_sufficiency_simulation/crop/entity/Profile.java
+++ backend/src/main/java/com/daniellaprade1/self_sufficiency_simulation/crop/entity/Profile.java
@@ -2,6 +2,8 @@ package com.daniellaprade1.self_sufficiency_simulation.crop.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 public class Profile {
 
@@ -16,4 +18,78 @@ public class Profile {
     @OneToOne
     @JoinColumn(name = "variety")
     private Variety variety;
+
+    public Profile() {
+    }
+
+    public Profile(Long id, float kcalPerGram, int yieldMinGrams, int yieldMaxGrams, Variety variety) {
+        this.id = id;
+        this.kcalPerGram = kcalPerGram;
+        this.yieldMinGrams = yieldMinGrams;
+        this.yieldMaxGrams = yieldMaxGrams;
+        this.variety = variety;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public float getKcalPerGram() {
+        return kcalPerGram;
+    }
+
+    public void setKcalPerGram(float kcalPerGram) {
+        this.kcalPerGram = kcalPerGram;
+    }
+
+    public int getYieldMinGrams() {
+        return yieldMinGrams;
+    }
+
+    public void setYieldMinGrams(int yieldMinGrams) {
+        this.yieldMinGrams = yieldMinGrams;
+    }
+
+    public int getYieldMaxGrams() {
+        return yieldMaxGrams;
+    }
+
+    public void setYieldMaxGrams(int yieldMaxGrams) {
+        this.yieldMaxGrams = yieldMaxGrams;
+    }
+
+    public Variety getVariety() {
+        return variety;
+    }
+
+    public void setVariety(Variety variety) {
+        this.variety = variety;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Profile profile = (Profile) o;
+        return Objects.equals(id, profile.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "id=" + id +
+                ", kcalPerGram=" + kcalPerGram +
+                ", yieldMinGrams=" + yieldMinGrams +
+                ", yieldMaxGrams=" + yieldMaxGrams +
+                ", variety=" + variety +
+                '}';
+    }
 }
--- backend/src/main/java/com/daniellaprade1/self_sufficiency_simulation/crop/entity/Variety.java
+++ backend/src/main/java/com/daniellaprade1/self_sufficiency_simulation/crop/entity/Variety.java
@@ -2,6 +2,7 @@ package com.daniellaprade1.self_sufficiency_simulation.crop.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -20,4 +21,61 @@ public class Variety {
 
     @OneToOne(mappedBy = "variety", cascade = CascadeType.ALL)
     private Profile profile;
+
+    public Variety() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Crop getCrop() {
+        return crop;
+    }
+
+    public void setCrop(Crop crop) {
+        this.crop = crop;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Variety variety = (Variety) o;
+        return Objects.equals(id, variety.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Variety{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", crop=" + crop +
+                ", profile=" + profile +
+                '}';
+    }
 }
