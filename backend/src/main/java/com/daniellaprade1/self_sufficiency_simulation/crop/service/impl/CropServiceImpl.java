718630db8390484bb64c6c8a3a71006b18455017
Daniel LaPrade <daniellaprade1@gmail.com>
2026-04-30 14:16:04 -0400

    Added postgres DB, connection verified

--- backend/src/main/java/com/daniellaprade1/self_sufficiency_simulation/crop/service/impl/CropServiceImpl.java
+++ backend/src/main/java/com/daniellaprade1/self_sufficiency_simulation/crop/service/impl/CropServiceImpl.java
@@ -2,9 +2,11 @@ package com.daniellaprade1.self_sufficiency_simulation.crop.service.impl;
 
 import com.daniellaprade1.self_sufficiency_simulation.crop.domain.dto.CropOptionDTO;
 import com.daniellaprade1.self_sufficiency_simulation.crop.service.CropService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CropServiceImpl implements CropService {
 
     // Unimplemented
